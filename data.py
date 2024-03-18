1d96af6f5feb15ed58b4b33f9037585b4494cbfe
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-03-18 17:50:36 +0200

    added: path of the data

---
+++ data.py
@@ -0,0 +1,76 @@
+movies = [
+    "The Dark Knight",
+    "Dune: Part Two",
+    "The Godfather",
+    "Schindler's List",
+    "Pulp Fiction",
+    "Whiplash",
+    "Alien",
+    "Memento",
+    "Once Upon a Time in America",
+    "Oppenheimer",
+    "Requiem for a Dream",
+    "Casino",
+    "No Country for Old Men",
+    "Barbie",
+    "Luxembourg, Luxembourg"
+]
+
+songs = [
+    ("The Pixies", "Where Is My Mind?"),
+    ("Lady Gaga", "Bad Romance"),
+    ("Pink Floyd", "Mother"),
+    ("The Animals", "The House of the Rising Sun"),
+    ("Nirvana", "Come as You Are"),
+    ("Daft Punk feat. Pharrell Williams", "Get Lucky"),
+    ("Bon Jovi", "Livin’ on a Prayer"),
+    ("The Mamas and the Papas", "‘California Dreamin"),
+    ("Mark Ronson feat. Bruno Mars", "Uptown Funk"),
+    ("Metallica", "Enter Sandman"),
+    ("Sex Pistols", "God Saves the Queen"),
+    ("Black Sabbath", "Iron Man"),
+    ("Pink Floyd", "Comfortably Numb"),
+    ("Radiohead", "Karma Police"),
+    ("Eric Clapton", "Layla")
+]
+
+video_games = [
+    {"Dark Souls": "rpg"},
+    {"Cyberpunk 2077": "rpg"},
+    {"Baldur's Gate III": "rpg"},
+    {"Elden Ring": "rpg"},
+    {"Serious Sam: The First Encounter": "shooter"},
+    {"Half-Life 2": "shooter"},
+    {"Metro Exodus": "shooter"},
+    {"Return to Castle Wolfenstein": "shooter"},
+    {"FIFA 23": "sports"},
+    {"Punch-Out!!": "sports"},
+    {"Wii Sports": "sports"},
+    {"Madden NFL 21": "sports"},
+    {"The Forest": "survival"},
+    {"Valheim": "survival"},
+    {"Rust": "survival"},
+    {"Minecraft": "survival"},
+    {"Red Dead Redemption 2": "adventure"},
+    {"Hogwarts Legacy": "adventure"},
+    {"God of War": "adventure"},
+    {"Star Wars Jedi: Survivor": "adventure"},
+    {"Civilization VI": "strategy"},
+    {"Crusader Kings III": "strategy"},
+    {"XCOM 2": "strategy"},
+    {"Age of Empires IV": "strategy"},
+    {"Forza Horizon 5": "racing"},
+    {"Gran Turismo 7": "racing"},
+    {"Need for Speed Heat": "racing"},
+    {"The Crew Motorfest": "racing"},
+    {"Sonic the Hedgehog 3": "platformer"},
+    {"Earthworm Jim 2": "platformer"},
+    {"Mega Man X": "platformer"},
+    {"Hollow Knight": "platformer"}
+]
+
+jokes = []
+
+stories = []
+
+
